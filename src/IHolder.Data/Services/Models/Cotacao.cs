1404e4841d2d63b62069c5d3d3442fa361fe4e1d
Diego Romario <diego.romario@outlook.com>
2020-06-27 05:59:47 -0300

    Adicionando calculo para variação e variação percentual na consulta de cotações

--- src/IHolder.Data/Services/Models/Cotacao.cs
+++ src/IHolder.Data/Services/Models/Cotacao.cs
@@ -18,8 +18,8 @@ namespace IHolder.Data.Services.Models
 
         public void CalcularVariacao ()
         {
-            //Variacao = Preco - PrecoAnterior;
-            //VariacaoPercentual = PrecoAnterior / Variacao * 100;
+            Variacao = Preco - PrecoAnterior;
+            VariacaoPercentual = Variacao / PrecoAnterior  * 100;
         }
 
         public decimal PrecoAnterior { get; private set; }
@@ -28,37 +28,6 @@ namespace IHolder.Data.Services.Models
         public decimal VariacaoPercentual { get; private set; }
 
     }
-
-    //public class CotacaoContract
-    //{
-    //    [JsonProperty("01. symbol")]
-    //    public string Ticker { get; set; }
-    //    [JsonProperty("02. open")]
-    //    public decimal Abertura { get; set; }
-    //    [JsonProperty("03. high")]
-    //    public decimal Maxima { get; set; }
-    //    [JsonProperty("04. low")]
-    //    public decimal Minima { get; set; }
-    //    [JsonProperty("05. price")]
-    //    public decimal Preco { get; set; }
-    //    [JsonProperty("06. volume")]
-    //    public decimal Volume { get; set; }
-    //    [JsonProperty("07. latest trading day")]
-    //    public string UltimoDataNegociacao { get; set; }
-    //    [JsonProperty("08. previous close")]
-    //    public decimal FechamentoAnterior { get; set; }
-    //    [JsonProperty("09. change")]
-    //    public decimal Variacao { get; set; }
-    //    [JsonProperty("10. change percent")]
-    //    public decimal VariacaoPercentual { get; set; }
-
-    //}
-
-    //public class CotacaoRoot
-    //{
-    //    [JsonProperty("Global Quote")]
-    //    public CotacaoContract Cotacao { get; set; }
-    //}
 
     public class ConsultaCotacaoArgs
     {
